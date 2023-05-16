7853a5c927e94a7b4d4432a1e2e654b820178186
Roger Twan <roger.twan@gmail.com>
2023-05-16 19:15:51 +0800

    :art: Replace kotlin to Java

---
+++ android/app/src/main/java/com/flutter/plain_launcher/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutter.plain_launcher;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/plain_launcher/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutter.plain_launcher
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
